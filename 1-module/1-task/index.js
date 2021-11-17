e5202ab392f38403f643057b97d76207510f5686
Darya Vyushkova <89069763121@mail.ru>
2021-11-17 20:20:22 +0200

    Изменила название переменной на более говорящее, добавила Number.isFinite для проверки на настоящее число в задаче 1 модуля 1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,15 +1,14 @@
 function factorial(n) {
-  // Почему бесконечность нечетная!???
-  // Провервяем: бесконечность, дробные и отрицательные числа.
-  if (n < 0 || n % 1 !== 0) {
+    // Проверяем: бесконечность, дробные и отрицательные числа.
+  if (n < 0 || n % 1 !== 0 || Number.isFinite(n) === false) {
     return NaN;
   }
 
   // Запускаем цикл, чтобы вычислить факториал.
-  let f = 1;
+  let factorial = 1;
   for (let i = 1; i <= n; i++ ) {
-    f = f * i;
+    factorial = factorial * i;
   }
 
-  return f;
+  return factorial;
 }
