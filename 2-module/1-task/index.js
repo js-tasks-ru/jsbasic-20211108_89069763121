0f289eb16861d9ec66aa1e2bf26dee73d384f586
Darya Vyushkova <89069763121@mail.ru>
2021-11-21 20:30:58 +0300

    Решила домашнее задание

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+let sum = 0;
+for (const key in salaries){
+  if(typeof(salaries[key])==='number' && Number.isFinite(salaries[key]) === true) {
+  sum = sum + salaries[key]
+  }
+}
+  return sum;
 }
